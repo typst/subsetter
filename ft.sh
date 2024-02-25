81895f0104ce45e00aa501162e5753c67f69e680
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2024-02-25 20:27:12 +0100

    add otf font

--- ft.sh
+++ ft.sh
@@ -1,4 +1,4 @@
-fonttools subset fonts/MPLUS1p-Regular.ttf --drop-tables=GSUB,GPOS,GDEF,FFTM,vhea,vmtx \
- --gids=0,1 --glyph-names --output-file=out_ft.ttf \
+fonttools subset fonts/LatinModernRoman-Regular.otf --drop-tables=GSUB,GPOS,GDEF,FFTM,vhea,vmtx \
+ --gids=1 --glyph-names --output-file=out_ft.otf \
  --notdef-outline --no-prune-unicode-ranges &&
-fonttools ttx -f -o out_ft.ttx out_ft.ttf
+fonttools ttx -f -o out_ft.ttx out_ft.otf
