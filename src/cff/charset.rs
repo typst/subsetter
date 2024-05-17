e85ce7c22886ac4b2f514333996a296122200b28
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2024-05-17 22:26:05 +0200

    Update gid_to_sid method

--- src/cff/charset.rs
+++ src/cff/charset.rs
@@ -13,25 +13,33 @@ pub(crate) enum Charset<'a> {
 }
 
 impl Charset<'_> {
-    // NOTE: Unlike the ttf-parser equivalent, this only returns SIDs for
-    // custom charsets
-    pub fn gid_to_sid(&self, gid: u16) -> Option<StringId> {
+    pub fn gid_to_sid(&self, gid: StringId) -> Option<StringId> {
         match self {
-            Charset::ISOAdobe => None,
-            Charset::Expert => None,
-            Charset::ExpertSubset => None,
+            Charset::ISOAdobe => {
+                if gid.0 <= 228 {
+                    Some(StringId(gid.0))
+                } else {
+                    None
+                }
+            }
+            Charset::Expert => {
+                EXPERT_ENCODING.get(usize::from(gid.0)).cloned().map(StringId)
+            }
+            Charset::ExpertSubset => {
+                EXPERT_SUBSET_ENCODING.get(usize::from(gid.0)).cloned().map(StringId)
+            }
             Charset::Format0(ref array) => {
-                if gid == 0 {
+                if gid.0 == 0 {
                     Some(StringId(0))
                 } else {
-                    array.get(gid - 1)
+                    array.get(gid.0 - 1)
                 }
             }
             Charset::Format1(array) => {
-                if gid == 0 {
+                if gid.0 == 0 {
                     Some(StringId(0))
                 } else {
-                    let mut sid = gid - 1;
+                    let mut sid = gid.0 - 1;
                     for range in *array {
                         if sid <= u16::from(range.left) {
                             sid = sid.checked_add(range.first.0)?;
@@ -45,10 +53,10 @@ impl Charset<'_> {
                 }
             }
             Charset::Format2(array) => {
-                if gid == 0 {
+                if gid.0 == 0 {
                     Some(StringId(0))
                 } else {
-                    let mut sid = gid - 1;
+                    let mut sid = gid.0 - 1;
                     for range in *array {
                         if sid <= range.left {
                             sid = sid.checked_add(range.first.0)?;
@@ -153,3 +161,29 @@ pub mod charset_id {
     pub const EXPERT: usize = 1;
     pub const EXPERT_SUBSET: usize = 2;
 }
+
+#[rustfmt::skip]
+const EXPERT_ENCODING: &[u16] = &[
+    0,    1,  229,  230,  231,  232,  233,  234,  235,  236,  237,  238,   13,   14,   15,   99,
+    239,  240,  241,  242,  243,  244,  245,  246,  247,  248,   27,   28,  249,  250,  251,  252,
+    253,  254,  255,  256,  257,  258,  259,  260,  261,  262,  263,  264,  265,  266,  109,  110,
+    267,  268,  269,  270,  271,  272,  273,  274,  275,  276,  277,  278,  279,  280,  281,  282,
+    283,  284,  285,  286,  287,  288,  289,  290,  291,  292,  293,  294,  295,  296,  297,  298,
+    299,  300,  301,  302,  303,  304,  305,  306,  307,  308,  309,  310,  311,  312,  313,  314,
+    315,  316,  317,  318,  158,  155,  163,  319,  320,  321,  322,  323,  324,  325,  326,  150,
+    164,  169,  327,  328,  329,  330,  331,  332,  333,  334,  335,  336,  337,  338,  339,  340,
+    341,  342,  343,  344,  345,  346,  347,  348,  349,  350,  351,  352,  353,  354,  355,  356,
+    357,  358,  359,  360,  361,  362,  363,  364,  365,  366,  367,  368,  369,  370,  371,  372,
+    373,  374,  375,  376,  377,  378,
+];
+
+/// The Expert Subset Encoding conversion as defined in the Adobe Technical Note #5176 Appendix C.
+#[rustfmt::skip]
+const EXPERT_SUBSET_ENCODING: &[u16] = &[
+    0,    1,  231,  232,  235,  236,  237,  238,   13,   14,   15,   99,  239,  240,  241,  242,
+    243,  244,  245,  246,  247,  248,   27,   28,  249,  250,  251,  253,  254,  255,  256,  257,
+    258,  259,  260,  261,  262,  263,  264,  265,  266,  109,  110,  267,  268,  269,  270,  272,
+    300,  301,  302,  305,  314,  315,  158,  155,  163,  320,  321,  322,  323,  324,  325,  326,
+    150,  164,  169,  327,  328,  329,  330,  331,  332,  333,  334,  335,  336,  337,  338,  339,
+    340,  341,  342,  343,  344,  345,  346
+];
