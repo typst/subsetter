2031d7d0441341758f11fc9a94d0316f65da74db
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2024-05-25 13:10:23 +0200

    CHange charset writing

--- src/cff/charset.rs
+++ src/cff/charset.rs
@@ -197,12 +197,14 @@ const EXPERT_SUBSET_ENCODING: &[u16] = &[
 /// we assign each glyph it's original glyph as the CID. This makes it easier to reference them
 /// from the PDF, since we know the CID a glyph will have before it's subsetted.
 pub fn rewrite_charset(gid_mapper: &GlyphRemapper, w: &mut Writer) -> Result<()> {
-    // We always use format 0.
-    w.write::<u8>(0);
+    if gid_mapper.num_gids() == 1 {
+        // We only have .notdef
+        w.write::<u8>(0);
 
-    // Skip .notdef since it's implicit.
-    for i in 1..gid_mapper.num_gids() {
-        w.write(i)
+    }   else {
+        w.write::<u8>(2);
+        w.write::<u16>(1);
+        w.write::<u16>(gid_mapper.num_gids() - 2);
     }
 
     Ok(())
