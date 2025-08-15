63de7558c469b918125193b4d6766393db18d36a
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2025-08-15 10:36:17 +0200

    Preserve more name records (#14)

--- src/name.rs
+++ src/name.rs
@@ -31,7 +31,9 @@ pub fn subset_table<'a>(table: &Table<'a>) -> Option<Table<'a>> {
         .iter()
         .copied()
         .filter(|record| {
-            record.is_unicode() && [0, 1, 2, 3, 4, 5, 6].contains(&record.name_id)
+            record.is_unicode()
+                && [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14]
+                    .contains(&record.name_id)
         })
         .collect::<Vec<_>>();
 
